e7404a0403a7229ed45bf86dd5cd63f5696fae68
Dapo-Obembe <obembedapo@gmail.com>
2025-10-01 12:33:09 +0100

    Added the function to add css class names to header and footer templates

--- inc/custom-functions/theme-updater.php
+++ inc/custom-functions/theme-updater.php
@@ -6,7 +6,7 @@
 if (is_admin()) {
     require_once get_template_directory() . '/class-custom-theme-updater.php';
     new CustomThemeUpdater(
-        'https://yourwebsite.com/wp-admin/admin-ajax.php?action=check_theme_update',
+        'https://www.dapoobembe.com/wp-admin/admin-ajax.php?action=check_theme_update',
         get_option('my_theme_license_key', '') // Optional
     );
 }
---
+++ inc/setup/add-class-to-header-footer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Add css class names to the header and footer template parts.
+ *
+ * @package SwiftPress
+ *
+ * @author Dapo Obembe <https://www.dapoobembe.com>
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Add custom CSS classes to template part wrappers.
+ *
+ * Filters the rendered output of template parts to add custom class names
+ * to the automatically generated wrapper elements. This allows for easier
+ * styling and targeting of header and footer template parts.
+ *
+ * @since 1.0.0
+ *
+ * @param string $block_content The block content about to be rendered.
+ * @param array  $block         The full block, including name and attributes.
+ *
+ * @return string Modified block content with additional CSS classes.
+ */
+function inr_add_template_part_class( $block_content, $block ) {
+	if ( isset( $block['attrs']['slug'] ) ) {
+		if ( 'header' === $block['attrs']['slug'] ) {
+			$block_content = str_replace( 'wp-block-template-part', 'wp-block-template-part site-header', $block_content );
+		}
+		if ( 'footer' === $block['attrs']['slug'] ) {
+			$block_content = str_replace( 'wp-block-template-part', 'wp-block-template-part site-footer', $block_content );
+		}
+	}
+	return $block_content;
+}
+add_filter( 'render_block_core/template-part', 'inr_add_template_part_class', 10, 2 );
